b56b0ce1d9ae1f0b1fb74c74d674dcf093053a33
Antoine Hugounet <antoine@hugounet.com>
2017-10-08 00:49:55 +0200

    stuff

--- Program/Program/earth_sun.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  earth_sun.cpp
-//  Program
-//
-//  Created by Antoine Hugounet on 06/10/2017.
-//  Copyright © 2017 Hugounet and Villeneuve. All rights reserved.
-//
-
-#include "earth_sun.hpp"
