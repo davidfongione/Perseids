fefbae7bd20600d3214cecd966923ff48e46f27d
Antoine Hugounet <antoine@hugounet.com>
2017-10-19 16:53:21 +0200

    Added a backup

---
+++ Program/Backup 1/Classes/planet.hpp
@@ -0,0 +1,48 @@
+//
+//  planet.hpp
+//  Program
+//
+//  Created by Antoine Hugounet on 08/10/2017.
+//  Copyright © 2017 Hugounet and Villeneuve. All rights reserved.
+//
+
+
+#pragma once
+#include <vector>
+#include <string>
+#include <fstream>
+
+
+class planet
+{
+public:
+    
+    planet(void);
+    planet(std::string name, double mass, double x, double y, double vx, double vy);
+    planet(const planet& other);    //  probably useless but more secure to prevent C++'s bad behaviors
+    
+    //  data members
+    std::vector<double> position;
+    std::vector<double> velocity;
+    
+    //  methods
+    int dim(void) const;
+    double distance(const planet& other) const;
+    double distance_center(void) const;
+    double grav_force(const planet& other) const;
+    double kinetic_energy(void) const;
+    double mass(void) const;
+    double potential_energy(const std::vector<planet>& system) const;
+    double total_energy(const std::vector<planet>& system) const;
+    void print(std::ofstream& file) const;
+    std::string name(void) const;
+
+    
+
+    
+private:
+    
+    int _dim;
+    double _mass;
+    std::string _name;
+};
