063ddb44a82c8bf9f6c43618af09d544b4f1fcf1
Antoine Hugounet <antoine@hugounet.com>
2017-10-26 15:28:17 +0200

    Updated results

--- Program/Program/main.cpp
+++ Program/Program/main.cpp
@@ -24,12 +24,12 @@ int main(int argc, const char* argv[])
         exit(1);
     }
 
-    string main_folder = "/Users/antoinehugounet/Documents/Scolarité/UiO/FYS3150 - Computational physics/Project 3/Perseids/Program/Résultats/Modèle à trois planètes/Vrai centre de masse/Outputs/";
+    string main_folder = "/Users/antoinehugounet/Documents/Scolarité/UiO/FYS3150 - Computational physics/Project 3/Perseids/Program/Résultats/Modèle à trois planètes/Faux centre de masse/";
     
     solver system;
-    system.add(earth_ejs_rmc);
-    system.add(jupiter_ejs_rmc);
-    system.add(sun_ejs_rmc);
+    system.add(earth_ejs_wmc);
+    system.add(jupiter_ejs_wmc);
+    system.add(sun_ejs_wmc);
     
     system.verlet(100., main_folder);
     
--- Program/Résultats/Modèle à trois planètes/Vrai centre de masse/Outputs/plot-png.gnu
+++
@@ -1,9 +0,0 @@
-reset
-set terminal png enhanced size 1000, 1000
-set output '/Users/antoinehugounet/Documents/Scolarité/UiO/FYS3150 - Computational physics/Project 3/Perseids/Program/Résultats/Modèle à trois planètes/Vrai centre de masse/Outputs/plot.png'
-set size ratio -1
-set title 'Time-step: 100.000000 years'
-set xlabel 'distance (AU)'
-set ylabel 'distance (AU)'
-
-plot '/Users/antoinehugounet/Documents/Scolarité/UiO/FYS3150 - Computational physics/Project 3/Perseids/Program/Résultats/Modèle à trois planètes/Vrai centre de masse/Outputs/sun' using 1:2 w l lt rgb "black" title 'sun'
